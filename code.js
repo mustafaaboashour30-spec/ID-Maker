0ce5356ae82ff5cf4d4b97181f24abd8753c16de
mustafaaboashour30-spec <mustafaaboashour30@gmail.com>
2026-02-01 20:43:57 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,10 @@
+
+console.log("hello");
+
+function code() {
+    document.getElementById("nameh").textContent = document.getElementById("namec").value
+    document.getElementById("ageh").textContent = document.getElementById("agec").value
+    document.getElementById("countryh").textContent = document.getElementById("countryc").value
+    document.getElementById("jobh").textContent = document.getElementById("jobc").value
+    document.getElementById("birthh").textContent = document.getElementById("birthc").value
+}
